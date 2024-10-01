9b243b0dbb5adf648da6e018cf85d2e85b8c39de
TanzidAnan <tanzid.anan125@gmail.com>
2024-10-01 17:46:18 +0600

    object

--- Explore-api/json.js
+++ Explore-api/json.js
@@ -1,4 +1,19 @@
 const user = { id: 1, name: 'Tanzid', job: 'web' };
 const stringified =JSON.stringify(user)
 // console.log(user)
-console.log(stringified)
+// console.log(stringified)
+
+const shop ={
+    owner: "Tanzi",
+    Address:{
+        street:'kockuhet voot er goli',
+        city: 'Dhaka',
+        country:'bd'
+    },
+    products:['laptop', 'mac', 'moniter','keybord'],
+    revenue: 45000,
+    isOpen:true,
+    isNew:false
+
+}
+console.log(shop)
