9955e0c7d7aab0d78a0fd4cbc5c34e5e0ec799f0
TanzidAnan <tanzid.anan125@gmail.com>
2024-10-01 23:36:58 +0600

    fetch

--- Explore-api/js/fecth.js
+++ Explore-api/js/fecth.js
@@ -1,3 +1,8 @@
-fetch('https://jsonplaceholder.org/users')
-      .then(response => response.json())
-      .then(json => console.log(json))
+// fetch('')
+//       .then(response =>console.log(response))
+//       .then(json => console.log(json))
+
+const url = 'https://jsonplaceholder.typicode.com/todos/1';
+fetch(url)
+.then(res => res.json())
+.then(json =>console.log(json))
