5ad22a19d8ffaa29a42d1ffb8963280517c499ba
TanzidAnan <tanzid.anan125@gmail.com>
2024-10-01 23:39:22 +0600

    funcation fatch

--- Explore-api/js/fecth.js
+++ Explore-api/js/fecth.js
@@ -3,6 +3,10 @@
 //       .then(json => console.log(json))
 
 const url = 'https://jsonplaceholder.typicode.com/todos/1';
-fetch(url)
-.then(res => res.json())
-.then(json =>console.log(json))
+
+function loadData() {
+    fetch(url)
+        .then(res => res.json())
+        .then(json => console.log(json))
+}
+
