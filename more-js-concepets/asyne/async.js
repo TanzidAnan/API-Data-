3be8ba5dd170a8d06c606f67839055479c880776
TanzidAnan <tanzid.anan125@gmail.com>
2024-10-03 01:57:47 +0600

    promise

---
+++ more-js-concepets/asyne/async.js
@@ -0,0 +1,13 @@
+const myLoader =() =>{
+    return new Promise((resolve,reject) =>{
+        const success =Math.random();
+        if(success <= 0.5){
+            resolve(success)
+        }
+        else{
+            reject(success)
+        }
+    })
+}
+myLoader()
+.then(data => console.log('data',data))
