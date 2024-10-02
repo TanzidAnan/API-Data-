8788a826296ab91c40d73fe1721dba6e0f171f74
TanzidAnan <tanzid.anan125@gmail.com>
2024-10-03 01:15:25 +0600

    setTime Out

--- more-js-concepets/async.js
+++ more-js-concepets/async.js
@@ -3,7 +3,10 @@
 
 console.log(1)
 console.log(2)
-setTimeout(doSomthing)
+setTimeout(() =>{
+    doSomthing();
+    console.log('Tanzid anan')
+},4000)
 console.log(4)
 console.log(5)
 console.log(6)
