1e14f719d50b99d512d707adee2e3bf923576a7b
TanzidAnan <tanzid.anan125@gmail.com>
2024-10-03 19:38:57 +0600

    arrow funcation

--- event-loop-js/js/await.js
+++ event-loop-js/js/await.js
@@ -10,3 +10,13 @@ async function loadUserAsycn(){
     const data = await res.json();
     console.log(data)
 }   
+
+const loadUserArrow = async() =>{
+    const res = await fetch('https://jsonplaceholder.typicode.com/users');
+    const data = await res.json();
+    console.log(data)
+}
+
+const loadUserCatch =async () =>{
+
+}
