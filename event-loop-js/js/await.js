00abe7247927284b583de37663a3176e01b2fd87
TanzidAnan <tanzid.anan125@gmail.com>
2024-10-03 19:44:40 +0600

    try catch

--- event-loop-js/js/await.js
+++ event-loop-js/js/await.js
@@ -18,5 +18,12 @@ const loadUserArrow = async() =>{
 }
 
 const loadUserCatch =async () =>{
+    try{
+        const res = await fetch('https://jsonplaceholder.typicode.com/users');
+        const data =await res.json();
+        console.log(data)
+    }
+    catch(err){
 
+    }
 }
