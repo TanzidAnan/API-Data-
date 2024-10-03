47f90e722805909cff054de7080b2163cf9da432
TanzidAnan <tanzid.anan125@gmail.com>
2024-10-03 17:26:04 +0600

    try catch

--- event-loop-js/js/try-catch.js
+++ event-loop-js/js/try-catch.js
@@ -1,20 +1,27 @@
-function checkAge(){
+function checkAge() {
     console.log(545);
-    const ageFild =document.getElementById('age');
-    const ageText =ageFild.value;
-    const errorTag =document.getElementById('error')
+    const ageFild = document.getElementById('age');
+    const ageText = ageFild.value;
+    const errorTag = document.getElementById('error')
     // console.log(age)
-    try{
-        const age =parseInt(ageText);
-        if(isNaN(age)){
-            console.log("age Not found ",age, ageText)
+    try {
+        const age = parseInt(ageText);
+        if (isNaN(age)) {
+            throw "Pleace Enter a Number"
         }
+        else if (age < 18) {
+            throw " Not allow"
+        }
+        else if(age> 30){
+            throw "A deak assain nah"
+        }
+        errorTag.innerHTML =" ";
     }
-    catch(err){
+    catch (err) {
         console.log('ERROR:', err);
-        errorTag.innerHTML ="someThing wrong"
+        errorTag.innerHTML = "ERROR :" + err
     }
-    finally{
+    finally {
         console.log('All done')
     }
     console.log(54545)
